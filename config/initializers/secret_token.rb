d38fe6767faa2b217b65f8a0082cee68532f067a
roca <RomelCampbell@gmail.com>
2012-11-17 13:22:14 -0500

    Creates the Initial rails starter app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railszor::Application.config.secret_token = '0be9910752c3f41634127c5d333a1348ba903325766f0b7830594561e304617fd610d36e728b865fc211779c69b12dba9f14933377855e8f347770f07d8bc350'
